46f29e2f15a3ec9a4ff150a0e76c18f4508194a9
Crazy-Ali <crazy.ali.no1@gmail.com>
2024-07-12 17:02:19 +0100

    4GK

--- m.py
+++ m.py
@@ -6,10 +6,10 @@ import os
 os.system("chmod +x *")
 
 # insert your Telegram bot token here
-bot = telebot.TeleBot('6116358973:AAHmZclg7OVPvpaBZCcHllkpD9ImGMqRV4M')
+bot = telebot.TeleBot('7325832756:AAGfYmTnuPn7pO_6VRLbIl3623jZE_LUqR8')
 
 # Admin user IDs
-admin_id = ["1967548493"]
+admin_id = ["7108670625"]
 
 # File to store allowed user IDs
 USER_FILE = "users.txt"
